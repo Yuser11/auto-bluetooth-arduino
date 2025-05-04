bdf5c80a4aa73824348080bb0323a67a756a99a4
Yuser11 <yuri.momessodelima@gmail.com>
2025-05-04 18:07:42 -0300

    ajuste

--- robo_movel/robo_movel.ino
+++ robo_movel/robo_movel.ino
@@ -1,10 +1,10 @@
 char t;
  
 void setup() {
-pinMode(13,OUTPUT);   //motor izquierdo delantero
-pinMode(12,OUTPUT);   //motor izquierdo trasero
-pinMode(11,OUTPUT);   //motor derecho delantero
-pinMode(10,OUTPUT);   //motor derecho trasero
+pinMode(4,OUTPUT);   //motor izquierdo delantero
+pinMode(5,OUTPUT);   //motor izquierdo trasero
+pinMode(6,OUTPUT);   //motor derecho delantero
+pinMode(7,OUTPUT);   //motor derecho trasero
 Serial.begin(9600);
 }
  
@@ -12,46 +12,48 @@ Serial.begin(9600);
  
 void frente(int tempo)
 {            //mover hacia adelante
-  digitalWrite(13,HIGH);
-  digitalWrite(11,HIGH);
-  delay(tempo)
+  digitalWrite(4,HIGH);
+  digitalWrite(6,HIGH);
+  delay(tempo);
+  digitalWrite(4,LOW);
+  digitalWrite(6,LOW);
 }
 void teste(int tempo)
 {
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(tempo)
+  delay(tempo);
   digitalWrite(LED_BUILTIN, LOW);
+  delay(tempo);
 
 }
  
 void re(int tempo)
 {      //mover hacia atras
-  digitalWrite(12,HIGH);
-  digitalWrite(10,HIGH);
-  delay(tempo)
+  digitalWrite(5,HIGH);
+  digitalWrite(7,HIGH);
+  delay(tempo);
+  digitalWrite(5,HIGH);
+  digitalWrite(7,HIGH);
 }
  
 void direita(int tempo)
 {      //girar a la derecha 
-  digitalWrite(11,HIGH);
-  delay(tempo)
+  digitalWrite(6,HIGH);
+  delay(tempo);
+  digitalWrite(6,LOW);
 }
  
-void frente(int tempo)
-{      //girar a la izquierda 
-  digitalWrite(13,HIGH);
-  delay(tempo)
-}
  
 void para(int tempo)
 {      //STOP 
-  digitalWrite(13,LOW);
-  digitalWrite(12,LOW);
-  digitalWrite(11,LOW);
-  digitalWrite(10,LOW);
-  delay(tempo)
+  digitalWrite(4,LOW);
+  digitalWrite(5,LOW);
+  digitalWrite(6,LOW);
+  digitalWrite(7,LOW);
+  delay(tempo);
 }
 void loop() 
 {
-teste(1000  )
+  digitalWrite(4,HIGH);
+  digitalWrite(6,HIGH);
 }
